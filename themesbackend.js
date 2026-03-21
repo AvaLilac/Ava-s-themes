4d5163be3af7adf9280c6dccf332757f21e8f2e6
AvaLilac <amyshimplays@gmail.com>
2026-03-21 12:28:40 -0400

    Add files via upload

---
+++ themesbackend.js
@@ -0,0 +1,10 @@
+{
+  "themes": [
+    {
+      "name": "Example Theme",
+      "author": "Example Author",
+      "preview": "https://www.example.com/preview.png",
+      "download": "https://www.example.com/theme.css"
+    }
+  ]
+}
