10e1d794b13afb5bd24d7ba4adf11f4271b421e8
AvaLilac <amyshimplays@gmail.com>
2026-03-23 16:34:43 -0400

    Update themesbackend.js

--- themesbackend.js
+++ themesbackend.js
@@ -2,9 +2,15 @@
   "themes": [
     {
       "name": "Custom Placeholder Theme",
-      "author": "AvaLilac",
+      "author": "AvaLilac#7432",
       "preview": "https://raw.githubusercontent.com/AvaLilac/Ava-s-themes/refs/heads/main/CustomPlaceHolder/preview.png",
       "download": "https://raw.githubusercontent.com/AvaLilac/Ava-s-themes/refs/heads/main/CustomPlaceHolder/CustomPlaceholderTheme.css"
+    },
+    {
+      "name": "Obsidian",
+      "author": "AvaLilac#7432",
+      "preview": "https://raw.githubusercontent.com/AvaLilac/Ava-s-themes/refs/heads/main/Obsidian/preview.png",
+      "download": "https://raw.githubusercontent.com/AvaLilac/Ava-s-themes/refs/heads/main/Obsidian/Obsidian.css"
     }
   ]
 }
