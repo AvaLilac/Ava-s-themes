bc69b74ab35755eb4d972568b29c7b5d79532445
AvaLilac <257690424+AvaLilac@users.noreply.github.com>
2026-07-06 01:05:11 -0400

    Add multiple theme entries to themesbackend.js

--- themesbackend.js
+++ themesbackend.js
@@ -23,6 +23,42 @@
       "author": "AvaLilac#7432",
       "preview": "https://raw.githubusercontent.com/AvaLilac/Ava-s-themes/refs/heads/main/background/preview.png",
       "download": "https://raw.githubusercontent.com/AvaLilac/Ava-s-themes/refs/heads/main/background/Background.css"
+    },
+    {
+      "name": "Status Card Fix",
+      "author": "AvaLilac#7432",
+      "preview": "https://raw.githubusercontent.com/AvaLilac/Ava-s-themes/refs/heads/main/statuscardfix/preview.png",
+      "download": "https://raw.githubusercontent.com/AvaLilac/Ava-s-themes/refs/heads/main/statuscardfix/Status_Card_fix.css"
+    },
+    {
+      "name": "Role icon Square Edges",
+      "author": "AvaLilac#7432",
+      "preview": "https://raw.githubusercontent.com/AvaLilac/Ava-s-themes/refs/heads/main/RoleiconSquareEdges/preview.png",
+      "download": "https://raw.githubusercontent.com/AvaLilac/Ava-s-themes/refs/heads/main/RoleiconSquareEdges/Role_Icon_Square_Edges.css"
+    },
+    {
+      "name": "Fix Attachment Issues",
+      "author": "AvaLilac#7432",
+      "preview": "https://raw.githubusercontent.com/AvaLilac/Ava-s-themes/refs/heads/main/fixattachmentissues/preview.png",
+      "download": "https://raw.githubusercontent.com/AvaLilac/Ava-s-themes/refs/heads/main/fixattachmentissues/fix_attachment_issues.css"
+    },
+    {
+      "name": "Catppuccin Macchiato For Toolbar, And echo (ONLINE THEME)",
+      "author": "AvaLilac#7432",
+      "preview": "https://raw.githubusercontent.com/AvaLilac/Ava-s-themes/refs/heads/main/catppuccinmacchiatofortoolbar/preview.png",
+      "download": "https://raw.githubusercontent.com/AvaLilac/Ava-s-themes/refs/heads/main/catppuccinmacchiatofortoolbar/Catppuccin_Macciato_For_ToolBar.css"
+    },
+    {
+      "name": "Title Exit Button Red",
+      "author": "AvaLilac#7432",
+      "preview": "https://raw.githubusercontent.com/AvaLilac/Ava-s-themes/refs/heads/main/TitleExitButtonRed/preview.png",
+      "download": "https://raw.githubusercontent.com/AvaLilac/Ava-s-themes/refs/heads/main/TitleExitButtonRed/Title_Exit_Button_Red.css"
+    },
+    {
+      "name": "Catppuccin Machiato Headliner Theming",
+      "author": "AvaLilac#7432",
+      "preview": "https://raw.githubusercontent.com/AvaLilac/Ava-s-themes/refs/heads/main/CatppuccinMachiatoHeadlinerTheming/preview.png",
+      "download": "https://raw.githubusercontent.com/AvaLilac/Ava-s-themes/refs/heads/main/CatppuccinMachiatoHeadlinerTheming/Catppuccin_Machiato_Headliner_Theming.css"
     }
   ]
 }
